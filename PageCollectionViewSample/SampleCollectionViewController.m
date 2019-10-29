1e1a4355693f5ab84730066512389f6513e35e25
Adam Wulf <adam.wulf@gmail.com>
2019-10-28 22:26:12 -0500

    Added buttons to demonstrate cell rotation

--- PageCollectionViewSample/SampleCollectionViewController.m
+++ PageCollectionViewSample/SampleCollectionViewController.m
@@ -9,43 +9,136 @@
 #import "SampleCollectionViewController.h"
 #import "SampleObject.h"
 
-@interface SampleCollectionViewController ()<MMPageCollectionViewDataSourceShelfLayout>
+
+@interface SampleCollectionViewController () <MMPageCollectionViewDataSourceShelfLayout>
+
+@property(nonatomic, strong) NSArray<SampleObject *> *objects;
+
+@property(nonatomic, strong) IBOutlet UIButton *rotateButton;
+@property(nonatomic, strong) IBOutlet UIButton *bumpButton;
+@property(nonatomic, strong) IBOutlet UIButton *resetButton;
 
 @end
 
+
 @implementation SampleCollectionViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    NSMutableArray<SampleObject *> *arr = [NSMutableArray array];
+
+    CGFloat fullWidth = CGRectGetWidth([[self collectionView] bounds]);
+    CGSize size = CGSizeMake(fullWidth, fullWidth);
+
+    // handle different size items similar to flow layout
+    size.height = 1.4 * size.width;
+
+    for (NSInteger i = 0; i < 20; i++) {
+        CGFloat rotation = 0;
+
+        if (i % 5 == 0) {
+            rotation = M_PI_2;
+        }
+
+        SampleObject *obj = [[SampleObject alloc] init];
+        [obj setIdealSize:size];
+        [obj setRotation:rotation];
+
+        [arr addObject:obj];
+    }
+
+    _objects = arr;
+}
 
 #pragma mark - UICollectionViewDataSource
 
 - (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
 {
+    // for this example, we'll just show the same collection three times
     return 3;
 }
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
 {
-    return 20;
+    return [_objects count];
+}
+
+#pragma mark - Actions
+
+- (IBAction)rotate:(id)sender
+{
+    CGPoint center = [[self collectionView] contentOffset];
+    center.x += [[self collectionView] bounds].size.width / 2;
+    center.y += [[self collectionView] bounds].size.height / 2;
+
+    NSIndexPath *indexPath = [[self collectionView] closestIndexPathForPoint:center];
+
+    SampleObject *obj = [_objects objectAtIndex:[indexPath row]];
+
+    [obj setRotation:[obj rotation] - M_PI_2];
+
+    MMPageLayout *layout = [[MMPageLayout alloc] initWithSection:[indexPath section]];
+    [layout setTargetIndexPath:indexPath];
+
+    [[self collectionView] setCollectionViewLayout:layout animated:YES];
+}
+
+- (IBAction)bump:(id)sender
+{
+    CGPoint center = [[self collectionView] contentOffset];
+    center.x += [[self collectionView] bounds].size.width / 2;
+    center.y += [[self collectionView] bounds].size.height / 2;
+
+    NSIndexPath *indexPath = [[self collectionView] closestIndexPathForPoint:center];
+
+    SampleObject *obj = [_objects objectAtIndex:[indexPath row]];
+
+    [obj setRotation:[obj rotation] - .1];
+
+    MMPageLayout *layout = [[MMPageLayout alloc] initWithSection:[indexPath section]];
+    [layout setTargetIndexPath:indexPath];
+
+    [[self collectionView] setCollectionViewLayout:layout animated:YES];
+}
+
+- (IBAction)reset:(id)sender
+{
+    CGPoint center = [[self collectionView] contentOffset];
+    center.x += [[self collectionView] bounds].size.width / 2;
+    center.y += [[self collectionView] bounds].size.height / 2;
+
+    NSIndexPath *indexPath = [[self collectionView] closestIndexPathForPoint:center];
+
+    SampleObject *obj = [_objects objectAtIndex:[indexPath row]];
+
+    [obj setRotation:0];
+
+    MMPageLayout *layout = [[MMPageLayout alloc] initWithSection:[indexPath section]];
+    [layout setTargetIndexPath:indexPath];
+
+    [[self collectionView] setCollectionViewLayout:layout animated:YES];
 }
 
 #pragma mark - Shelf Layout
 
-- (id<MMShelfLayoutObject>)collectionView:(UICollectionView *)collectionView layout:(MMShelfLayout *)collectionViewLayout objectAtIndexPath:(NSIndexPath *)indexPath{
-    CGFloat fullWidth = CGRectGetWidth([collectionView bounds]);
-    CGSize size = CGSizeMake(fullWidth, fullWidth);
-    CGFloat rotation = 0;
-    
-    // handle different size items similar to flow layout
-    size.height = 1.4 * size.width;
+- (id<MMShelfLayoutObject>)collectionView:(UICollectionView *)collectionView layout:(MMShelfLayout *)collectionViewLayout objectAtIndexPath:(NSIndexPath *)indexPath
+{
+    return [_objects objectAtIndex:[indexPath row]];
+}
 
-    if(indexPath.row % 5 == 0){
-        rotation = M_PI_2;
+- (void)collectionView:(UICollectionView *)collectionView didChangeToLayout:(UICollectionViewLayout *)newLayout fromLayout:(UICollectionViewLayout *)oldLayout
+{
+    if ([newLayout isMemberOfClass:[MMPageLayout class]]) {
+        [[self rotateButton] setHidden:NO];
+        [[self bumpButton] setHidden:NO];
+        [[self resetButton] setHidden:NO];
+    } else {
+        [[self rotateButton] setHidden:YES];
+        [[self bumpButton] setHidden:YES];
+        [[self resetButton] setHidden:YES];
     }
-    
-    SampleObject *obj = [[SampleObject alloc] init];
-    [obj setIdealSize:size];
-    [obj setRotation:rotation];
-
-    return obj;
 }
 
 @end
